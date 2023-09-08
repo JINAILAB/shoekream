4666687aefecc5f13563caf122e48dc0d6e366fe
JINAILAB <parksonice@naver.com>
2023-09-08 09:24:16 +0900

    [Feat] database 생성
    - database를 생성하는 sql 파일 추가

---
+++ database/base_database.sql
@@ -0,0 +1,33 @@
+CREATE TABLE shoes (
+    id INT PRIMARY KEY,
+    shoe_name VARCHAR(255) NOT NULL,
+    category VARCHAR(255),
+    original_price_with_currency INT,
+    date_released DATE,
+    colorway VARCHAR(255),
+    gender ENUM('male', 'female', 'unisex'),
+    brand VARCHAR(255),
+    wish_count INT DEFAULT 0,
+    review_count INT DEFAULT 0,
+    image_url_right VARCHAR(512),
+    image_url_left VARCHAR(512),
+    image_url_top VARCHAR(512),
+    image_dir_right VARCHAR(512),
+    image_dir_left VARCHAR(512),
+    image_dir_top VARCHAR(512)
+);
+
+CREATE TABLE shoe_prices (
+    price_id INT PRIMARY KEY AUTO_INCREMENT,
+    shoe_id INT,
+    price INT,
+    date_created DATETIME,
+    FOREIGN KEY (shoe_id) REFERENCES shoes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE style_image (
+    image_id INT PRIMARY KEY AUTO_INCREMENT,
+    shoe_id INT,
+    image_url VARCHAR(512),
+    FOREIGN KEY (shoe_id) REFERENCES shoes(id) ON DELETE CASCADE
+)
